50fe473a339b246998ed39bcdbad6f9467302143
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-12 13:59:33 -0600

    Add base costumes.

---
+++ P5R.CostumeFramework/Models/Costume.cs
@@ -0,0 +1,29 @@
+﻿namespace P5R.CostumeFramework.Models;
+
+internal enum Costume
+{
+    Default,
+    Summer_Uniform,
+    Winter_Uniform,
+    Summer_Clothes,
+    Winter_Clothes,
+    Tracksuit,
+    Swimsuit,
+    Loungewear,
+    Gekkoukan_High,
+    Yasogami_High,
+    St_Hermelin_High,
+    Vincents_OutFit,
+    Karukozaka_High,
+    Butler_Suit,
+    Christmas_Outfit,
+    Dancewear,
+    Shadow_Ops_Uniform,
+    Samurai_Garb,
+    Starlight_Outfit,
+    Ultramarin_Outfit,
+    Featherman_Suit,
+    Demonica_Head,
+    Demonica_Suit,
+    New_Cinema_Outfit,
+}

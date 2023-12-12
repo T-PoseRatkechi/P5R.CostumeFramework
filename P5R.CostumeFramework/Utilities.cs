3643d2c28e458d54f8ebc79307307e90e3e7aa76
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-12 04:57:47 -0600

    Add project files.

---
+++ P5R.CostumeFramework/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace P5R.CostumeFramework;
+
+internal static class Utilities
+{
+    public static string PushCallerRegisters = "push rcx\npush rdx\npush r8\npush r9";
+    public static string PopCallerRegisters = "pop r9\npop r8\npop rdx\npop rcx";
+    public static nint BaseAddress = Process.GetCurrentProcess().MainModule?.BaseAddress ?? 0;
+
+    public static ushort ToBigEndian(this ushort value)
+    {
+        var bigEndianValue = BitConverter.ToUInt16(BitConverter.GetBytes(value).Reverse().ToArray());
+        return bigEndianValue;
+    }
+}

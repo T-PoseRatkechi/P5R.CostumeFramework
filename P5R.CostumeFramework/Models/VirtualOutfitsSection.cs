10c6f77c7cf2ceae23dfc9814a1f5c143050aee7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-23 23:27:55 -0600

    Add virtual outfits section.

---
+++ P5R.CostumeFramework/Models/VirtualOutfitsSection.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace P5R.CostumeFramework.Models;
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct VirtualOutfitsSection
+{
+    private const int MAX_OUTFITS = 286;
+
+    public VirtualOutfitsSection()
+    {
+        var outfits = new OutfitEntry[MAX_OUTFITS];
+        var size = sizeof(OutfitEntry) * outfits.Length;
+        this.size = ((uint)size).ToBigEndian();
+
+        fixed (byte* ptr = outfitsBuffer)
+        {
+            for (int i = 0; i < outfits.Length; i++)
+            {
+                var outfit = outfits[i];
+
+                outfit.icon = 4;
+                outfit.unknown9 = 100;
+                outfit.unknown11 = 20;
+                outfit.unknown12 = 799;
+                outfit.equippableFlags = EquippableUsers.Joker;
+
+                Marshal.StructureToPtr(
+                    outfit,
+                    (nint)(ptr + (sizeof(OutfitEntry) * i)),
+                    false);
+            }
+        }
+    }
+
+    public uint size;
+    public fixed byte outfitsBuffer[MAX_OUTFITS * 32];
+}

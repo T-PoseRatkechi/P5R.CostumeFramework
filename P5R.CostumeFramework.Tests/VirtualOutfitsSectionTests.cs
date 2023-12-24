7d4270fd0fa80f600dad08836261da28caf121be
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-23 23:29:26 -0600

    Add testing project.

---
+++ P5R.CostumeFramework.Tests/VirtualOutfitsSectionTests.cs
@@ -0,0 +1,16 @@
+﻿using P5R.CostumeFramework.Models;
+
+namespace P5R.CostumeFramework.Tests;
+
+public class VirtualOutfitsSectionTests
+{
+    [Fact]
+    public void OutfitsSection_Works()
+    {
+        var outfits = new VirtualOutfitsSection();
+        using var file = File.OpenWrite("./test-outfits.tbl");
+        using var writer = new BinaryWriter(file);
+        writer.Write(outfits.size);
+        //writer.Write(outfits.outfitsBuffer);
+    }
+}

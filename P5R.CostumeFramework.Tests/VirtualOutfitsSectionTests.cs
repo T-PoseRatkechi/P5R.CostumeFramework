96c18ceded402b0928d5eb229ce746a36afbcc75
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-24 19:30:13 -0600

    Update test.

--- P5R.CostumeFramework.Tests/VirtualOutfitsSectionTests.cs
+++ P5R.CostumeFramework.Tests/VirtualOutfitsSectionTests.cs
@@ -1,16 +1,23 @@
 ﻿using P5R.CostumeFramework.Models;
+using System.Runtime.InteropServices;
 
 namespace P5R.CostumeFramework.Tests;
 
 public class VirtualOutfitsSectionTests
 {
     [Fact]
-    public void OutfitsSection_Works()
+    public unsafe void OutfitsSection_Works()
     {
         var outfits = new VirtualOutfitsSection();
-        using var file = File.OpenWrite("./test-outfits.tbl");
+        var buffer = new byte[sizeof(VirtualOutfitsSection)];
+
+        fixed (byte* ptr = buffer)
+        {
+            Marshal.StructureToPtr(outfits, (nint)ptr, false);
+        }
+
+        using var file = File.Create("./test-outfits.tbl");
         using var writer = new BinaryWriter(file);
-        writer.Write(outfits.size);
-        //writer.Write(outfits.outfitsBuffer);
+        writer.Write(buffer);
     }
 }

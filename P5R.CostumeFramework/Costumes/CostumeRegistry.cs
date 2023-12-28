fa03c42855505f3a0c55ae88dd1a029be804dcc0
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-28 03:18:37 -0600

    Update costume registry and add binding external files used by costumes.

--- P5R.CostumeFramework/Costumes/CostumeRegistry.cs
+++ P5R.CostumeFramework/Costumes/CostumeRegistry.cs
@@ -13,9 +13,9 @@ internal class CostumeRegistry
 {
     private readonly IModLoader modLoader;
     private readonly ICriFsRedirectorApi criFsApi;
-    private readonly MessageScriptCompiler compiler;
+    private readonly CostumeFactory costumeFactory;
 
-    private readonly List<Costume> costumes = new();
+    private readonly GameCostumes costumes = new();
 
     public CostumeRegistry(IModLoader modLoader)
     {
@@ -25,34 +25,23 @@ internal class CostumeRegistry
 
         AtlusEncoding.SetCharsetDirectory(Path.Join(modLoader.GetDirectoryForModId("P5R.CostumeFramework"), "Charsets"));
         LibraryLookup.SetLibraryPath(Path.Join(modLoader.GetDirectoryForModId("P5R.CostumeFramework"), "Libraries"));
-        this.compiler = new(FormatVersion.Version1BigEndian, AtlusEncoding.Persona5RoyalEFIGS)
+        var compiler = new MessageScriptCompiler(FormatVersion.Version1BigEndian, AtlusEncoding.Persona5RoyalEFIGS)
         {
             Library = LibraryLookup.GetLibrary("p5r")
         };
 
-        var characters = Enum.GetValues<Character>();
-        for (int currentSet = 0; currentSet < VirtualOutfitsSection.GAME_OUTFIT_SETS + VirtualOutfitsSection.MOD_OUTFIT_SETS; currentSet++)
-        {
-            foreach (var character in characters)
-            {
-                var itemId = 0x7010 + (currentSet * 10) + (int)character - 1;
-                this.costumes.Add(new(character, itemId));
-            }
-        }
+        this.costumeFactory = new(criFsApi, compiler, this.costumes);
     }
 
-    public Costume[] GetModCostumes()
-        => this.costumes.Where(x => this.IsActiveModCostume(x.ItemId)).ToArray();
-
-    public bool TryGetModCostume(int itemId, out Costume? costume)
+    public bool TryGetModCostume(int itemId, out Costume costume)
     {
-        costume = this.costumes.FirstOrDefault(x => x.ItemId == itemId && x.ReplacementFilePath != null);
+        costume = this.costumes.FirstOrDefault(x => x.ItemId == itemId && x.GmdFilePath != null)!;
         return costume != null;
     }
 
     public bool IsActiveModCostume(int itemId)
         => VirtualOutfitsSection.IsModOutfit(itemId)
-        && this.costumes.FirstOrDefault(x => x.ItemId == itemId)?.ReplacementFilePath != null;
+        && this.costumes.FirstOrDefault(x => x.ItemId == itemId)?.GmdFilePath != null;
 
     private void OnModLoading(IModV1 mod, IModConfigV1 config)
     {
@@ -61,73 +50,36 @@ internal class CostumeRegistry
             return;
         }
 
-        // Scan for replacement files for each character.
         var modDir = this.modLoader.GetDirectoryForModId(config.ModId);
+        this.AddBindFiles(modDir);
+
+        // Register mod costumes.
         foreach (var character in Enum.GetValues<Character>())
         {
-            var costumesDir = Path.Join(modDir, "costumes", character.ToString());
-            if (!Directory.Exists(costumesDir))
+            var characterDir = Path.Join(modDir, "costumes", character.ToString());
+            if (!Directory.Exists(characterDir))
             {
                 continue;
             }
 
-            foreach (var file in Directory.EnumerateFiles(costumesDir, "*.gmd", SearchOption.TopDirectoryOnly))
+            foreach (var file in Directory.EnumerateFiles(characterDir, "*.gmd", SearchOption.TopDirectoryOnly))
             {
-                var relativePath = Path.GetRelativePath(modDir, file);
-                var modCostume = this.GetAvailableModCostume(character);
-                if (modCostume == null)
-                {
-                    Log.Warning($"No available costume slots for: {character}");
-                    continue;
-                }
-
-                modCostume.Name = Path.GetFileNameWithoutExtension(file);
-                modCostume.ReplacementFilePath = file;
-                modCostume.ReplacementBindPath = relativePath;
-
-                var descriptionFile = Path.ChangeExtension(file, ".msg");
-                if (File.Exists(descriptionFile))
-                {
-                    if (compiler.TryCompile(File.ReadAllText(descriptionFile), out var messageScript))
-                    {
-                        using var ms = new MemoryStream();
-                        messageScript.ToStream(ms, true);
-                        modCostume.DescriptionMessageBinary = ms.ToArray();
-                    }
-                    else
-                    {
-                        Log.Warning($"Failed to compile costume description.\nFile: {descriptionFile}");
-                    }
-
-                }
-
-                var musicFile = Path.ChangeExtension(file, ".pme");
-                if (File.Exists(musicFile))
-                {
-                    modCostume.MusicScriptFile = musicFile;
-                }
-
-                Log.Information($"Character: {modCostume.Character} || Item ID: {modCostume.ItemId} || Bind: {modCostume.ReplacementBindPath}");
-                this.criFsApi.AddBindCallback(context =>
-                {
-                    context.RelativePathToFileMap[$@"R2\{relativePath}"] = new()
-                    {
-                        new()
-                        {
-                            FullPath = file,
-                            LastWriteTime = DateTime.UtcNow,
-                            ModId = "Costume Framework",
-                        },
-                    };
-                });
+                this.costumeFactory.CreateCostume(modDir, character, file);
             }
         }
     }
 
-    private Costume? GetAvailableModCostume(Character character)
-        => this.costumes
-        .FirstOrDefault(x =>
-        x.Character == character
-        && VirtualOutfitsSection.IsModOutfit(x.ItemId)
-        && x.ReplacementFilePath == null);
+    private void AddBindFiles(string modDir)
+    {
+        var bindDir = Path.Join(modDir, "costumes", "bind");
+        if (Directory.Exists(bindDir))
+        {
+            foreach (var file in Directory.EnumerateFiles(bindDir, "*", SearchOption.AllDirectories))
+            {
+                var relativeFilePath = Path.GetRelativePath(bindDir, file);
+                this.criFsApi.AddBind(file, relativeFilePath, "Costume Framework");
+                Log.Debug($"Costume file binded: {relativeFilePath}");
+            }
+        }
+    }
 }

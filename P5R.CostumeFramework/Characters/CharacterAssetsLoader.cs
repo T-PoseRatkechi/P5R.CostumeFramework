f8709046d691ee533fd79bd6222f64690fa0b9b2
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-03 00:27:24 -0600

    Add loading configurable character assets.

---
+++ P5R.CostumeFramework/Characters/CharacterAssetsLoader.cs
@@ -0,0 +1,55 @@
+﻿using CriFs.V2.Hook.Interfaces;
+using P5R.CostumeFramework.Models;
+using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+
+namespace P5R.CostumeFramework.Characters;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+internal static class CharacterAssetsLoader
+{
+    private static IModLoader modLoader;
+    private static ICriFsRedirectorApi criFsApi;
+    private static CharacterAssetsSettings assetSettings;
+
+    public static void Init(IModLoader modLoader, CharacterAssetsSettings assetSettings)
+    {
+        CharacterAssetsLoader.modLoader = modLoader;
+        CharacterAssetsLoader.assetSettings = assetSettings;
+        modLoader.GetController<ICriFsRedirectorApi>().TryGetTarget(out criFsApi!);
+        modLoader.ModLoading += OnModLoading;
+    }
+
+    private static void OnModLoading(IModV1 mod, IModConfigV1 config)
+    {
+        foreach (var setting in assetSettings)
+        {
+            if (setting.Value != CharacterAssets.Default)
+            {
+                AddCharacterAssets(config, setting.Key, setting.Value);
+            }
+        }
+    }
+
+    private static void AddCharacterAssets(IModConfigV1 modConfig, Character character, CharacterAssets assets)
+    {
+        var modDir = modLoader.GetDirectoryForModId(modConfig.ModId);
+        var assetsDir = GetCharacteAssetsDir(character, assets, modDir);
+
+        if (!Directory.Exists(assetsDir))
+        {
+            return;
+        }
+
+        foreach (var file in Directory.EnumerateFiles(assetsDir, "*", SearchOption.AllDirectories))
+        {
+            var relativePath = Path.GetRelativePath(assetsDir, file);
+            criFsApi.AddBind(file, relativePath, "Costume Framework");
+        }
+
+        Log.Debug($"Added character assets: {character} || {assets} || {modConfig.ModName}");
+    }
+
+    private static string GetCharacteAssetsDir(Character character, CharacterAssets assets, string modDir)
+        => Path.Join(modDir, "characters", character.ToString(), assets.ToString("d"));
+}
--- P5R.CostumeFramework/Config.cs
+++ P5R.CostumeFramework/Config.cs
@@ -1,4 +1,5 @@
-﻿using P5R.CostumeFramework.Template.Configuration;
+﻿using P5R.CostumeFramework.Characters;
+using P5R.CostumeFramework.Template.Configuration;
 using System.ComponentModel;
 
 namespace P5R.CostumeFramework.Configuration;
@@ -13,6 +14,46 @@ public class Config : Configurable<Config>
     [Description("Costume will randomize when moving between areas.")]
     [DefaultValue(false)]
     public bool RandomizeCostumes { get; set; } = false;
+
+    [Category("Character Assets")]
+    [DisplayName("Joker")]
+    public CharacterAssets Joker_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Ryuji")]
+    public CharacterAssets Ryuji_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Morgana")]
+    public CharacterAssets Morgana_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Ann")]
+    public CharacterAssets Ann_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Yusuke")]
+    public CharacterAssets Yusuke_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Makoto")]
+    public CharacterAssets Makoto_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Haru")]
+    public CharacterAssets Haru_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Futaba")]
+    public CharacterAssets Futaba_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Akechi")]
+    public CharacterAssets Akechi_Assets { get; set; }
+
+    [Category("Character Assets")]
+    [DisplayName("Sumire")]
+    public CharacterAssets Sumire_Assets { get; set; }
 
     [DisplayName("Overworld Costumes")]
     [Description("Costumes will apply in the overworld too.\nThis is just a for fun feature, expect some non-game breaking visual bugs.")]

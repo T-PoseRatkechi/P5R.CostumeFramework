feb1fbc6cebd52a47da7d0c0f273b7f0bf6a5e2e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-05 08:03:42 -0600

    Fix Futaba AOA goodbye.

--- P5R.CostumeFramework/Hooks/GoodbyeHook.cs
+++ P5R.CostumeFramework/Hooks/GoodbyeHook.cs
@@ -4,7 +4,6 @@ using p5rpc.lib.interfaces;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.X64;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
-using System.Runtime.InteropServices;
 using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
 
 namespace P5R.CostumeFramework.Hooks;
@@ -12,7 +11,7 @@ namespace P5R.CostumeFramework.Hooks;
 internal class GoodbyeHook
 {
     [Function(Register.rbx, Register.rax, true)]
-    private delegate nint GoodbyeHookFunction(int characterId);
+    private delegate nint GoodbyeHookFunction(ushort characterId);
     private IReverseWrapper<GoodbyeHookFunction>? goodbyeWrapper;
     private IAsmHook? goodbyeHook;
 
@@ -47,11 +46,14 @@ internal class GoodbyeHook
         });
     }
 
-    private nint GoodbyeHookImpl(int characterId)
+    private nint GoodbyeHookImpl(ushort characterId)
     {
-        var character =
-            characterId == 61969 ? Character.Futaba
-            : (Character)(characterId + 1);
+        var character = (Character)(characterId + 1);
+        if (!Enum.IsDefined(character))
+        {
+            character = Character.Futaba;
+        }
+
         Log.Debug($"Getting Goodbye BMD for: {character}");
 
         var outfitItemId = this.p5rLib.FlowCaller.GET_EQUIP((int)character, (int)EquipSlot.Costume);

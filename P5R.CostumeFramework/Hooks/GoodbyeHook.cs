f02055ac397a2cfc1e6c1b324c9d8708b1b008a1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-07 08:44:56 -0600

    Fix hook not detecting Akechi's AOA correctly.

--- P5R.CostumeFramework/Hooks/GoodbyeHook.cs
+++ P5R.CostumeFramework/Hooks/GoodbyeHook.cs
@@ -4,21 +4,22 @@ using p5rpc.lib.interfaces;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.X64;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using System.Runtime.InteropServices;
 using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
 
 namespace P5R.CostumeFramework.Hooks;
 
 internal class GoodbyeHook
 {
-    [Function(Register.rbx, Register.rax, true)]
-    private delegate nint GoodbyeHookFunction(ushort characterId);
+    [Function(Register.rdx, Register.rax, true)]
+    private delegate nint GoodbyeHookFunction(nint goodbyeStrPtr);
     private IReverseWrapper<GoodbyeHookFunction>? goodbyeWrapper;
     private IAsmHook? goodbyeHook;
 
     private readonly IP5RLib p5rLib;
     private readonly CostumeRegistry costumes;
 
-    private readonly Dictionary<int, nint> goodbyeCache = new();
+    private readonly Dictionary<nint, Character> goodbyeCache = new();
 
     public GoodbyeHook(
         IStartupScanner scanner,
@@ -46,12 +47,13 @@ internal class GoodbyeHook
         });
     }
 
-    private nint GoodbyeHookImpl(ushort characterId)
+    private nint GoodbyeHookImpl(nint goodbyeStrPtr)
     {
-        var character = (Character)(characterId + 1);
-        if (!Enum.IsDefined(character))
+        if (!this.goodbyeCache.TryGetValue(goodbyeStrPtr, out var character))
         {
-            character = Character.Futaba;
+            var goodbyeStr = Marshal.PtrToStringAnsi(goodbyeStrPtr)!;
+            character = GetCharacter(goodbyeStr);
+            this.goodbyeCache[goodbyeStrPtr] = character;
         }
 
         Log.Debug($"Getting Goodbye BMD for: {character}");
@@ -60,18 +62,26 @@ internal class GoodbyeHook
         if (this.costumes.TryGetCostume(outfitItemId, out var costume)
             && costume.GoodbyeBindPath != null)
         {
-            if (this.goodbyeCache.TryGetValue(outfitItemId, out var cachedPtr))
-            {
-                return cachedPtr;
-            }
-            else
-            {
-                var ptr = StringsCache.GetStringPtr(costume.GoodbyeBindPath);
-                this.goodbyeCache[outfitItemId] = ptr;
-                return ptr;
-            }
+            return StringsCache.GetStringPtr(costume.GoodbyeBindPath);
         }
 
         return IntPtr.Zero;
     }
+
+    private static Character GetCharacter(string goodbyeFile)
+        => goodbyeFile switch
+        {
+            "battle/event/BCD/goodbye/bksk_hero.BCD" => Character.Joker,
+            "battle/event/BCD/goodbye/bksk_reiji.BCD" => Character.Ryuji,
+            "battle/event/BCD/goodbye/bksk_morgana.BCD" => Character.Morgana,
+            "battle/event/BCD/goodbye/bksk_an.BCD" => Character.Ann,
+            "battle/event/BCD/goodbye/bksk_yugo.BCD" => Character.Yusuke,
+            "battle/event/BCD/goodbye/bksk_makoto.BCD" => Character.Makoto,
+            "battle/event/BCD/goodbye/bksk_ranko.BCD" => Character.Haru,
+            "battle/event/BCD/goodbye/bksk_futaba.BCD" => Character.Futaba,
+            "battle/event/BCD/goodbye/bksk_ranko.BCD"
+            or "battle/event/BCD/goodbye/bksk_aketi_b.BCD" => Character.Akechi,
+            "battle/event/BCD/goodbye/bksk_uzuki.BCD" => Character.Sumire,
+            _ => Character.Joker,
+        };
 }

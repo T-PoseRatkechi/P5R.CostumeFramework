38e03f78029948bb30ed422c57e5cca445f90c0a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-15 05:15:41 -0600

    Add support for file emulators in character assets.

--- P5R.CostumeFramework/Characters/CharacterAssetsLoader.cs
+++ P5R.CostumeFramework/Characters/CharacterAssetsLoader.cs
@@ -1,7 +1,10 @@
-﻿using CriFs.V2.Hook.Interfaces;
+﻿using BF.File.Emulator.Interfaces;
+using CriFs.V2.Hook.Interfaces;
 using P5R.CostumeFramework.Models;
+using PAK.Stream.Emulator.Interfaces;
 using Reloaded.Mod.Interfaces;
 using Reloaded.Mod.Interfaces.Internal;
+using SPD.File.Emulator.Interfaces;
 
 namespace P5R.CostumeFramework.Characters;
 
@@ -10,6 +13,9 @@ internal static class CharacterAssetsLoader
 {
     private static IModLoader modLoader;
     private static ICriFsRedirectorApi criFsApi;
+    private static ISpdEmulator spdApi;
+    private static IBfEmulator bfApi;
+    private static IPakEmulator pakApi;
     private static CharacterAssetsSettings assetSettings;
 
     public static void Init(IModLoader modLoader, CharacterAssetsSettings assetSettings)
@@ -17,6 +23,9 @@ internal static class CharacterAssetsLoader
         CharacterAssetsLoader.modLoader = modLoader;
         CharacterAssetsLoader.assetSettings = assetSettings;
         modLoader.GetController<ICriFsRedirectorApi>().TryGetTarget(out criFsApi!);
+        modLoader.GetController<ISpdEmulator>().TryGetTarget(out spdApi!);
+        modLoader.GetController<IBfEmulator>().TryGetTarget(out bfApi!);
+        modLoader.GetController<IPakEmulator>().TryGetTarget(out pakApi!);
         modLoader.ModLoading += OnModLoading;
     }
 
@@ -41,8 +50,31 @@ internal static class CharacterAssetsLoader
             return;
         }
 
+        var spdDir = Path.Join(assetsDir, "femulator", "spd");
+        if (Directory.Exists(spdDir))
+        {
+            spdApi.AddDirectory(spdDir);
+        }
+
+        var bfDir = Path.Join(assetsDir, "femulator", "bf");
+        if (Directory.Exists(bfDir))
+        {
+            bfApi.AddDirectory(bfDir);
+        }
+
+        var pakDir = Path.Join(assetsDir, "femulator", "pak");
+        if (Directory.Exists(pakDir))
+        {
+            pakApi.AddDirectory(pakDir);
+        }
+
         foreach (var file in Directory.EnumerateFiles(assetsDir, "*", SearchOption.AllDirectories))
         {
+            if (file.Contains("femulator", StringComparison.OrdinalIgnoreCase))
+            {
+                continue;
+            }
+
             var relativePath = Path.GetRelativePath(assetsDir, file);
             criFsApi.AddBind(file, relativePath, "Costume Framework");
         }
